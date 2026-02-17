8be4be45c502ef7074d196bd83c54db87b6945df
lntra <fernandoaraujo1202@gmail.com>
2026-02-16 23:32:18 -0300

    alteração

--- src/main/java/com/gildedrose/SulfurasUpdater.java
+++ src/main/java/com/gildedrose/SulfurasUpdater.java
@@ -3,7 +3,6 @@ package com.gildedrose;
 class SulfurasUpdater implements ItemUpdater {
     @Override
     public void update(Item item) {
-        // Sulfuras never has to be sold nor does it decrease in Quality
-        // So it doesn't change at all
+        // Sulfuras ainda não possuem regras de atualização, então não é necessário implementar nada aqui.
     }
 }
